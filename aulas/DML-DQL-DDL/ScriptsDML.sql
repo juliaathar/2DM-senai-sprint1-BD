06f926b1cb61dba2d6037caebb69e84ef5c1160c
juliaathar <juathar@gmail.com>
2023-08-01 07:54:55 -0300

    ...

---
+++ aulas/DML-DQL-DDL/ScriptsDML.sql
@@ -0,0 +1,11 @@
+--INSERE DADOS NA TABELA
+INSERT INTO Funcionarios (Nome)
+VALUES ('Paulo')
+
+--ALTERAR DADOS NA TABELA
+UPDATE Funcionarios
+SET Nome = 'Wender' WHERE IdFuncionario = 2
+
+-- EXCLUIR DADOS NA TABELA
+DELETE FROM Funcionarios 
+WHERE IdFuncionario = 2
