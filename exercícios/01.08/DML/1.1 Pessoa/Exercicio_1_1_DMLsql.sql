ec438795e949c69b60ebb5cc350b7940ac1579d0
juliaathar <juathar@gmail.com>
2023-08-01 09:04:09 -0300

    exercicio 1.1 feito

---
+++ exercícios/01.08/DML/1.1 Pessoa/Exercicio_1_1_DMLsql.sql
@@ -0,0 +1,8 @@
+INSERT INTO Pessoa (Nome, CNH)
+VALUES ('Júlia','12345678901')
+
+INSERT INTO Email (IdPessoa, Endereco)
+VALUES (1, 'julia@gmail.com')
+
+INSERT INTO Telefone (IdPessoa, Numero)
+VALUES (1, 1198277354)
